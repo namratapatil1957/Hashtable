48196e3b8469f029c0c4f97d2b0867786dde8fde
namratapatil1957 <namratapatil1957@gmail.com>
2023-02-01 16:29:58 +0530

    Frequency of words in a sentence

--- src/com/bridgelabz/hashtable/hashTable.java
+++ src/com/bridgelabz/hashtable/hashTable.java
@@ -3,71 +3,39 @@ import java.util.*;
 
 public class hashTable {
 
-
-
-	void word_frequency(String str)
-    {	
-
-	void word_frequency(String str1)
-    {
-        
-
-	Map<String,Integer> m = new HashMap<>();
+	void word_frequency(String str){
+		
+	
+		Map<String,Integer> m = new HashMap<>();
  
-        String array[] = str1.split(" ");
+        String array[] = str.split(" ");
  
-        for(int i = 0; i < array.length; i++)
-        {
+        for(int i = 0; i < array.length; i++){
 
-            if(m.containsKey(array[i]))
-            {
+            if(m.containsKey(array[i])){
                 m.put(array[i], m.get(array[i])+1);
             }
-            else
-            {
+            else{
                 m.put(array[i],1);
             }
             
         }
         
-        for(Map.Entry<String,Integer> entry: m.entrySet())
-        {
-            System.out.println( entry.getKey()  + " is " + entry.getValue() + " time ");
+        for(Map.Entry<String,Integer> entry: m.entrySet()){
+            
+        	System.out.println( entry.getKey()  + " is " + entry.getValue() + " time ");
         
         }
-
-         
-
-        System.out.println(" \n ");
-        
-        for (int j = 0; j < array.length; j++) 
-        {
-            if (m.containsKey("avoidable")) 
-            {
-                m.remove("avoidable");
-            }
-
-        }
-        System.out.println(" After removing: \n");
-        for(Map.Entry<String,Integer> entry: m.entrySet())
-        {
-            System.out.println( entry.getKey()  + " is " + entry.getValue() + " time ");
-        
-        }
-        
-        
 
     }
 
-	
     public static void main(String[] args) {
         
     	hashTable table = new hashTable();
     	
     	String str = "To be or not to be";
-    	String str1 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
     	
-        table.word_frequency(str1);
+        table.word_frequency(str);
         
     }
 
