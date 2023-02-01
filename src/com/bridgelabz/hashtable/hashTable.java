a2a754ade168557ac95ee007e6fb1a98d6a128c3
namratapatil1957 <namratapatil1957@gmail.com>
2023-02-01 14:46:24 +0530

    Frequency of words in a sentence

--- src/com/bridgelabz/hashtable/hashTable.java
+++ src/com/bridgelabz/hashtable/hashTable.java
@@ -3,39 +3,43 @@ import java.util.*;
 
 public class hashTable {
 
-	void word_frequency(String str, String st)
+	void word_frequency(String str)
     {
         
 		Map<String,Integer> m = new HashMap<>();
  
         String array[] = str.split(" ");
  
-        for(int i = 0; i < array.length; i++)
-        {
+        for(int i = 0; i < array.length; i++) {
 
-            if(m.containsKey(array[i]))
-            {
+            if(m.containsKey(array[i])){
                 m.put(array[i], m.get(array[i])+1);
             }
             else
             {
                 m.put(array[i],1);
             }
+            
+        }
+
+        for(Map.Entry<String,Integer> entry: m.entrySet()){
+            System.out.println( entry.getKey()  + " is " + entry.getValue() + " time ");
+        
         }
         
-        for(Map.Entry<String,Integer> entry: m.entrySet())
-        {
-            System.out.println(entry.getKey() + " is " + entry.getValue() + " time ");
-        }
+        
     }
 
 	
     public static void main(String[] args) {
         
     	hashTable table = new hashTable();
-    	String str = "To be or not to be";
-    	String st = "Namrata namrata";
     	
-        table.word_frequency(str, st);
+    	String str = "To be or not to be";
+    	
+    	
+        table.word_frequency(str);
+        
     }
+
 }
